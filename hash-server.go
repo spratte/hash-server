b3d72d6c970888d195f99e214681cf46125ae097
Michael Spratte <Michael.Spratte@hpe.com>
2017-08-20 17:09:10 -0600

    All functionality is now in

--- hash-server.go
+++ hash-server.go
@@ -10,9 +10,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"regexp"
 	"strconv"
 	"sync"
+	"sync/atomic"
+	"syscall"
 	"time"
 )
 
@@ -28,14 +31,18 @@ type Stats struct {
 	Total      int    `json:"total"`
 	Average    int    `json:"average"`
 	mutex      sync.Mutex
-	totalMsecs uint64
+	sum        time.Duration
 }
 
-func (s *Stats) Update(msecs uint64) {
+//
+// Update the statistics with the specified elapsed time. The counter
+// of hashes is incremented.
+//
+func (s *Stats) Update(elapsed time.Duration) {
 	s.mutex.Lock()
 	s.Total++
-	s.totalMsecs += msecs
-	s.Average = int(float64(s.totalMsecs)/float64(stats.Total) + 0.5)
+	s.sum += elapsed
+	s.Average = int(float64(s.sum)/float64(stats.Total)/float64(time.Millisecond) + 0.5)
 	s.mutex.Unlock()
 }
 
@@ -46,18 +53,29 @@ var (
 	//
 	hashMutex   sync.Mutex
 	hashes      []*HashedPassword
+	shutdownReq int32	// True if shutdown has been requested
 
 	stats       Stats
 	server      *http.Server
+	sigchan     chan os.Signal
 	waitgroup   sync.WaitGroup
 )
 
 //
 // checkMethod logs a message if the request method does not match the
-// input parameter 'method'. If the method matches true will be
-// returned, otherwise false.
+// input parameter 'method'. It also checks if a shutdown is
+// pending. If the method matches and no shutdown is pending, true
+// will be returned, otherwise false.
 //
 func checkMethod(w http.ResponseWriter, req *http.Request, method string) bool {
+	//
+	// Check for shutdown pending
+	//
+	if atomic.LoadInt32(&shutdownReq) != 0 {
+		sendResponse(w, req, http.StatusServiceUnavailable,
+			"<p>Server shutting down<p>/r/n")
+		return false
+	}
 	if req.Method != method {
 		sendResponse(w, req, http.StatusMethodNotAllowed, "")
 		return false
@@ -106,7 +124,9 @@ func sendResponse(w http.ResponseWriter, req *http.Request, code int, data strin
 	if f, ok := w.(http.Flusher); ok {
 		f.Flush()
 	}
-
+	//
+	// Log a message similar to what Apache httpd does
+	//
 	log.Printf("%s %s %s %d %d %s\n",
 		req.RemoteAddr,
 		req.Method,
@@ -118,12 +138,37 @@ func sendResponse(w http.ResponseWriter, req *http.Request, code int, data strin
 }
 
 //
+// Perform a graceful shutdown
+//
+func shutdown() {
+	atomic.StoreInt32(&shutdownReq, 1)
+	waitgroup.Wait()	// Wait for everyone to finish up
+        ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+        defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalln("Failed to close server listener - ", err)
+	}
+}
+
+//
 // Process /hash POSTs
 //
 func HashStart(w http.ResponseWriter, req *http.Request) {
 
 	waitgroup.Add(1)	// Flag that we're in here
 	defer waitgroup.Done()	// and flag that we're done on exit
+	//
+	// Gather request timing information. Unfortunately, the stats
+	// are quite boring because hashing a small amount of data
+	// takes less than a millisecond on an old computer, e.g., a
+	// 32-bit 2.4GHz Pentium 4. If we look at the microseconds, or
+	// increase the password size to a few megabytes, things are a
+	// bit more interesting.
+	//
+	start := time.Now()	// Start timer
+	defer func() {
+		stats.Update(time.Since(start))
+	} ()
 
 	if !checkMethod(w, req, "POST") { return }
 
@@ -148,12 +193,10 @@ func HashStart(w http.ResponseWriter, req *http.Request) {
 	hashMutex.Lock()
 	id := len(hashes)	// The id will be the index of the next entry
 	hashes = append(hashes, new(HashedPassword))
-	hashes[id].wg.Add(1)	// Mark that we're this hash is not ready
+	hashes[id].wg.Add(1)	// Mark that this hash is not ready
 	hashMutex.Unlock()
 	h := hashes[id]
 	h.pw = "$Hashing in progress$"
-	fmt.Println(h)
-	
 	sendResponse(w, req, http.StatusOK, strconv.Itoa(id) + "\r\n")
 	log.Printf("Hashing request:  %d", id)
 	time.Sleep(5 * time.Second)
@@ -163,11 +206,9 @@ func HashStart(w http.ResponseWriter, req *http.Request) {
 	//
 	sha512 := sha512.New()
 	sha512.Write([]byte(password[0]))
-	//fmt.Println(len(pwb), pwb)
 	h.pw = base64.StdEncoding.EncodeToString(sha512.Sum(nil))
 	h.wg.Done()	// Flag that hashing is done
-	stats.Update(2)
-	log.Printf("Hashing complete: %d", id)
+	log.Printf("Hashing complete: %d %s", id, time.Since(start))
 }
 
 //
@@ -183,7 +224,6 @@ func HashFetch(w http.ResponseWriter, req *http.Request) {
 	//
 	// Parse out the hash ID
 	//
-	fmt.Println(req.Method, req.URL.Path, req.Form) //XXX
 	regexp := regexp.MustCompile(`/hash/(\d+)$`)
 	matches := regexp.FindStringSubmatch(req.URL.Path)
 	if len(matches) != 2 {
@@ -224,57 +264,68 @@ func HashStats(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Println("JSON marshal error:", err)
 	}
-	fmt.Println(stats, b)
-	os.Stdout.Write(b)
 	sendResponse(w, req, http.StatusOK, string(b))
 }
 
 //
-// Process /shutdown
+// Process /shutdown POST
 //
 func Shutdown(w http.ResponseWriter, req *http.Request) {
 
-	sendResponse(w, req, http.StatusOK, "/shutdown\r\n")
+	waitgroup.Add(1)	// Flag that we're in here
+	defer waitgroup.Done()	// and flag that we're done on exit
+
+	if !checkMethod(w, req, "POST") { return }
+
+	sendResponse(w, req, http.StatusOK, "<p>Shutdown requested\r\n")
 	log.Printf("Shutdown received")
-
-	waitgroup.Wait()	// Wait for everyone to finish up
-        ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
-        defer cancel()
-	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalln("Failed to close server listener - ", err)
-	}
-}
-
-func Hello(w http.ResponseWriter, req *http.Request) {
-
-	waitgroup.Add(1)	// Flag that we're in here
-	defer waitgroup.Done()	// and flag that we're done on exit
-
-	fmt.Println(req)
-	io.WriteString(w, "Hello, world!\r\n")
-}
-
-func Pause(w http.ResponseWriter, req *http.Request) {
-
-	waitgroup.Add(1)	// Flag that we're in here
-	defer waitgroup.Done()	// and flag that we're done on exit
-
-	fmt.Println(req)
-	time.Sleep(10 * time.Second)
-	io.WriteString(w, "/pause\r\n")
+	//
+	// Send SIGINT and let the signal handler catch it. On Linux we can do:
+	//
+	//	syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+	//
+	// But Windows does not have syscall.Kill, so a somewhat tacky but portable way is to
+	// just put the SIGINT into the signal channel created at startup.
+	//
+	sigchan<- syscall.SIGINT
 }
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <port>\n", os.Args[0])
+		os.Exit(1)
+	}
+	port := os.Args[1]
+	//
+	// Initialize shutdown flag
+	//
+	atomic.StoreInt32(&shutdownReq, 0)
 
 	// Register handlers
-	http.HandleFunc("/hello", Hello)
 	http.HandleFunc("/hash",  HashStart)
 	http.HandleFunc("/hash/", HashFetch)
 	http.HandleFunc("/stats", HashStats)
 	http.HandleFunc("/shutdown", Shutdown)
-	http.HandleFunc("/pause", Pause)
 
-	server = &http.Server{Addr: ":8080"}
-	err := server.ListenAndServe()
-	log.Printf("Server exiting: %s", err)
+	sigchan = make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGHUP)
+
+	server = &http.Server{Addr: ":" + port}
+	//
+	// Start server
+	//
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Printf("Server exiting: %s", err)
+			os.Exit(1)
+		}
+	} ()
+	//
+	// Wait for SIGINT or SIGHUP and then call shutdown
+	//
+	sig := <-sigchan
+	log.Printf("Received %s signal", sig)
+	shutdown()
+	log.Println("Shutdown complete")
 }
