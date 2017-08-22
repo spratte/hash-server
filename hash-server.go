eba9e7c18ecd9e8170ae7a02b082dd60b3765838
Michael Spratte <Michael.Spratte@hpe.com>
2017-08-21 18:04:58 -0600

    Created HashStore to abstract storage methods

--- hash-server.go
+++ hash-server.go
@@ -20,11 +20,77 @@ import (
 )
 
 //
-// Array of hashes indexed by job identifier
+// Array of hashes indexed by job ID
 //
 type HashedPassword struct {
 	pw string
 	wg sync.WaitGroup
+	done int32	// 0 = has not been set
+}
+
+//
+// Hash store to hold hashes as they are created
+//
+type HashStore struct {
+	mutex  sync.RWMutex	// Used to synchronize access to "hashes"
+	hashes []*HashedPassword
+}
+
+//
+// Allocate a hash store entry and return the job ID.
+//
+func (hs *HashStore) Alloc() int {
+	hs.mutex.Lock()
+	defer hs.mutex.Unlock()
+
+	id := len(hs.hashes)	// The id will be the index of the next entry
+	hs.hashes = append(hs.hashes, new(HashedPassword))
+	h := hs.hashes[id]
+	h.pw = "$Uninitialized Entry$"
+	h.wg.Add(1)	// Mark that this hash entry is not ready
+	return id
+}
+
+//
+// Set hash store entry with a new hash value. This function can be
+// called only once. Subsequent calls will cause are fatal. This
+// avoids a panic in the call to Done() which then locks everything
+// up. This function assumes that the hash ID is valid and was
+// returned by HashStore.Alloc.
+//
+func (hs *HashStore) SetHash(id int, hashString string) {
+
+	hs.mutex.RLock()	// Lock the hash store while we're checking the ID
+
+	//
+	// Validate ID and die if it's not valid
+	//
+	if id < 0 || id >= len(hs.hashes) {
+		log.Fatalf("Fatal SetHash job ID %d not found", id)
+	}
+	h := hs.hashes[id]
+	if atomic.SwapInt32(&h.done, 1) != 0 {
+		log.Fatalf("Fatal duplicate call to SetHash for ID %d", id)
+	}
+	hs.mutex.RUnlock()
+
+	h.pw = hashString
+	h.wg.Done()     // Signal that hashing is done
+}
+
+//
+// Get hashed password from job ID
+//
+func (hs *HashStore) GetHash(id int) (string, string) {
+	hs.mutex.RLock()
+	defer hs.mutex.RUnlock()
+
+	if id < 0 || id >= len(hs.hashes) {
+		return "", "Job ID not found\r\n"
+	}
+	h := hs.hashes[id]
+	h.wg.Wait()	// Wait for hashing to complete
+	return h.pw, ""
 }
 
 type Stats struct {
@@ -42,18 +108,14 @@ func (s *Stats) Update(elapsed time.Duration) {
 	s.mutex.Lock()
 	s.Total++
 	s.sum += elapsed
-	s.Average = int(float64(s.sum)/float64(stats.Total)/float64(time.Millisecond) + 0.5)
+	s.Average = int(float64(s.sum)/float64(stats.Total)/
+		float64(time.Millisecond) + 0.5)
 	s.mutex.Unlock()
 }
 
 var (
-	//
-	// The hash mutex is used when adding a new entry and fetching
-	// an entry.
-	//
-	hashMutex   sync.Mutex
-	hashes      []*HashedPassword
-	shutdownReq int32	// True if shutdown has been requested
+	hashStore   HashStore
+	shutdownReq int32	// non-zero if shutdown has been requested
 
 	stats       Stats
 	server      *http.Server
@@ -67,7 +129,9 @@ var (
 // pending. If the method matches and no shutdown is pending, true
 // will be returned, otherwise false.
 //
-func checkMethod(w http.ResponseWriter, req *http.Request, method string) bool {
+func checkMethod(w http.ResponseWriter,
+	req *http.Request,
+	method string) bool {
 	//
 	// Check for shutdown pending
 	//
@@ -97,7 +161,10 @@ func checkMethod(w http.ResponseWriter, req *http.Request, method string) bool {
 //
 //	data - String containing body of the response
 //
-func sendResponse(w http.ResponseWriter, req *http.Request, code int, data string) {
+func sendResponse(w http.ResponseWriter,
+	req *http.Request,
+	code int, data string) {
+
         if code != http.StatusOK {
                 http.Error(w,
                         req.Method + " " + http.StatusText(code),
@@ -161,9 +228,12 @@ func HashStart(w http.ResponseWriter, req *http.Request) {
 	// Gather request timing information. Unfortunately, the stats
 	// are quite boring because hashing a small amount of data
 	// takes less than a millisecond on an old computer, e.g., a
-	// 32-bit 2.4GHz Pentium 4. If we look at the microseconds, or
-	// increase the password size to a few megabytes, things are a
-	// bit more interesting.
+	// 15-year-old 32-bit 2.4GHz Pentium 4. If we look at the
+	// microseconds, or increase the password size to a few
+	// megabytes, things are a bit more interesting.
+	//
+	// Move the following 4 lines wherever the request timing
+	// should start.
 	//
 	start := time.Now()	// Start timer
 	defer func() {
@@ -187,16 +257,9 @@ func HashStart(w http.ResponseWriter, req *http.Request) {
 	}
 
 	//
-	// Allocate a new hash entry. Use the hash mutex to allow only
-	// one goroutine to use the hash array at a time.
+	// Allocate a new hash entry.
 	//
-	hashMutex.Lock()
-	id := len(hashes)	// The id will be the index of the next entry
-	hashes = append(hashes, new(HashedPassword))
-	hashes[id].wg.Add(1)	// Mark that this hash is not ready
-	hashMutex.Unlock()
-	h := hashes[id]
-	h.pw = "$Hashing in progress$"
+	id := hashStore.Alloc()
 	sendResponse(w, req, http.StatusOK, strconv.Itoa(id) + "\r\n")
 	log.Printf("Hashing request:  %d", id)
 	time.Sleep(5 * time.Second)
@@ -206,8 +269,8 @@ func HashStart(w http.ResponseWriter, req *http.Request) {
 	//
 	sha512 := sha512.New()
 	sha512.Write([]byte(password[0]))
-	h.pw = base64.StdEncoding.EncodeToString(sha512.Sum(nil))
-	h.wg.Done()	// Flag that hashing is done
+	hashStore.SetHash(id,
+		base64.StdEncoding.EncodeToString(sha512.Sum(nil)))
 	log.Printf("Hashing complete: %d %s", id, time.Since(start))
 }
 
@@ -228,26 +291,25 @@ func HashFetch(w http.ResponseWriter, req *http.Request) {
 	matches := regexp.FindStringSubmatch(req.URL.Path)
 	if len(matches) != 2 {
 		sendResponse(w, req, http.StatusBadRequest,
-			"Malformed URL - must be /hash/<id> where <id> is a number")
+			"Malformed URL - must be /hash/<id>\r\n")
 		return
 	}
 	//
 	// Extract ID and validate it
 	//
 	id, err := strconv.Atoi(matches[1])
-	hashMutex.Lock()
-	if err != nil || id >= len(hashes) {
-		hashMutex.Unlock()
-		sendResponse(w, req, http.StatusBadRequest, "Invalid hash id")
+	if err != nil {
+		sendResponse(w, req, http.StatusBadRequest,
+			"Malformed hash job ID\r\n")
 		return
 	}
-	hashMutex.Unlock()
-	h := hashes[id]
-	//
-	// Wait for hashing to complete
-	//
-	h.wg.Wait()	// For hashing to complete
-	sendResponse(w, req, http.StatusOK, h.pw)
+
+	if hashString, errString := hashStore.GetHash(id); errString != "" {
+		sendResponse(w, req, http.StatusBadRequest, errString)
+	} else {
+		sendResponse(w, req, http.StatusOK, hashString + "\r\n")
+	}
+	return
 }
 
 //
@@ -264,7 +326,7 @@ func HashStats(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Println("JSON marshal error:", err)
 	}
-	sendResponse(w, req, http.StatusOK, string(b))
+	sendResponse(w, req, http.StatusOK, string(b) + "\r\n")
 }
 
 //
@@ -280,12 +342,14 @@ func Shutdown(w http.ResponseWriter, req *http.Request) {
 	sendResponse(w, req, http.StatusOK, "<p>Shutdown requested\r\n")
 	log.Printf("Shutdown received")
 	//
-	// Send SIGINT and let the signal handler catch it. On Linux we can do:
+	// Send SIGINT and let the signal handler catch it. On Linux
+	// we can do:
 	//
 	//	syscall.Kill(syscall.Getpid(), syscall.SIGINT)
 	//
-	// But Windows does not have syscall.Kill, so a somewhat tacky but portable way is to
-	// just put the SIGINT into the signal channel created at startup.
+	// But Windows does not have syscall.Kill, so a somewhat tacky
+	// but portable way is to just put the SIGINT into the signal
+	// channel created at startup.
 	//
 	sigchan<- syscall.SIGINT
 }
